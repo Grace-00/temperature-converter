716e6c2becad4d0329411167e1e13bffdab9d286
GraziaPalombella <GraziaPalombella@users.noreply.github.com>
2020-08-14 22:49:24 +0200

    elements stored in variables + DOMContentLoaded

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+    //store useful elements in variables
+    const form = document.querySelector("form");
+    const button = document.getElementById("submit");
+    const result = document.getElementById("result");
+    const userInput = document.getElementById("userInput");
+  
